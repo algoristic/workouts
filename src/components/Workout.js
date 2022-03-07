c99f5dd492267a4d9be306ba4bb7b9de42ac5483
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-07 14:51:30 +0100

    Improve readability: Workout

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -4,7 +4,6 @@ import Subtitle from './Subtitle'
 import DateTimeService from '../service/dateTimeService'
 import ParameterCollector from '../service/parameterCollector'
 import ParameterService from '../service/parameterService'
-import { categories } from '../assets/categories.json'
 import { config } from '../assets/app.config.json'
 import { plans } from '../assets/plans.json'
 import { programs } from '../assets/programs.json'
@@ -27,62 +26,82 @@ const getPath = (string) => {
     return path;
 };
 
+const getPlanControl = (plan, dateTime) => {
+    const level = new ParameterService('level').value();
+    let step = new ParameterService('step').value();
+    let type = plans[plan][step].types;
+    step = parseInt(step);
+    const maxSteps = plans[plan].length;
+    let nextStep = (1 + step);
+    if(nextStep > (maxSteps - 1)) {
+        nextStep = 0;
+    }
+    const rerollCollector = new ParameterCollector(config.allWorkoutParams);
+    const rerollPath = '?app=forward' + rerollCollector.getSearchString();
+    return {
+        next: `?app=finish&plan=${plan}&step=${nextStep}&t=${dateTime.getNow()}`,
+        reroll: rerollPath,
+        subtitle: `Typ '${getTypeString(type)}' und Level '${getLevelString(level)}'`
+    };
+};
+
+const getProgramControl = (program, dateTime) => {
+    let step = new ParameterService('step').value();
+    step = parseInt(step);
+    const maxSteps = programs[program];
+    let nextStep = (1 + step);
+    if(nextStep > maxSteps) {
+        nextStep = 1;
+    }
+    return {
+        next: `?app=finish&program=${program}&step=${nextStep}&t=${dateTime.getNow()}`
+    };
+};
+
+const getSelectionControl = (dateTime) => {
+    const level = new ParameterService('level').value();
+    let type = new ParameterService('type').value();
+    const rerollCollector = new ParameterCollector(config.allWorkoutParams);
+    const rerollPath = '?app=forward' + rerollCollector.getSearchString();
+    return {
+        next: `?app=finish&t=${dateTime.getNow()}`,
+        reroll: rerollPath,
+        subtitle: `Typ '${getTypeString(type)}' und Level '${getLevelString(level)}'`
+    };
+};
+
 const Workout = () => {
     const dateTime = new DateTimeService();
     //build path for workout instructions image
     const workout = new ParameterService('s').value();
-    const plan = new ParameterService('plan').value();
-    const program = new ParameterService('program').value();
-    let step = new ParameterService('step').value();
     const decoded = decode(workout);
     const path = getPath(decoded);
-    const level = new ParameterService('level').value();
-    let type = new ParameterService('type').value();
 
-    //build path for next
-    let app = 'finish'
-    let nextParams = [];
-    let additional = '';
-    step = parseInt(step);
+    const plan = new ParameterService('plan').value();
+    const program = new ParameterService('program').value();
+    let control = undefined;
     if(plan) {
-        nextParams = ['plan'];
-        const maxSteps = plans[plan].length;
-        let nextStep = (1 + step);
-        if(nextStep > (maxSteps - 1)) {
-            nextStep = 0;
-        }
-        additional = `&step=${nextStep}`;
-        type = plans[plan][step].types;
+        control = getPlanControl(plan, dateTime);
     } else if(program) {
-        nextParams = ['program'];
-        const maxSteps = programs[program];
-        let nextStep = (1 + step);
-        if(nextStep > maxSteps) {
-            nextStep = 1;
-        }
-        additional = `&step=${nextStep}`;
+        control = getProgramControl(program, dateTime);
+    } else {
+        control = getSelectionControl(dateTime);
     }
-    additional += `&t=${dateTime.getNow()}`;
-    const paramSearchString = new ParameterCollector(nextParams).getSearchString();
-    const nextPath = `?app=${app}` + paramSearchString + additional;
 
-    //build path for reroll
-    const rerollCollector = new ParameterCollector(config.allWorkoutParams);
-    const rerollPath = '?app=forward' + rerollCollector.getSearchString();
     return (
         <div className='workout-wrapper d-flex flex-column align-items-center'>
             <div className='d-flex flex-column' style={{ maxWidth: '576px' }}>
                 <Header>Dein Workout:</Header>
                 <img className='img-fluid img-thumbnail' alt='Workout' src={path} />
-                <Button href={nextPath} color='success' icon='🏁' text='Fertig' classes='my-3' />
+                <Button href={control.next} color='success' icon='🏁' text='Fertig' classes='my-3' />
                 {
                     !program && (
-                        <Button href={rerollPath} color='primary' classes='mb-3' icon='🔄' text='Anderes Workout' />
+                        <Button href={control.reroll} color='primary' classes='mb-3' icon='🔄' text='Anderes Workout' />
                     )
                 }
                 {
                     !program && (
-                        <Subtitle text={`Typ '${getTypeString(type)}' und Level '${getLevelString(level)}'`} />
+                        <Subtitle text={control.subtitle} />
                     )
                 }
             </div>
