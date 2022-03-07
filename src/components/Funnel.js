1de50b149247d17f7617dfdf60ff8ed70d24fa61
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-07 18:24:50 +0100

    Remove unnecessary

--- src/components/Funnel.js
+++ src/components/Funnel.js
@@ -6,8 +6,6 @@ import ParameterCollector from '../service/parameterCollector'
 import all from '../assets/categories.json'
 import { config } from '../assets/app.config.json'
 
-import './Funnel.css'
-
 const Funnel = ({ question, start, category, next, parameter, pickup }) => {
     const funnel = all.categories[category];
     let parameters = '?';
@@ -17,42 +15,40 @@ const Funnel = ({ question, start, category, next, parameter, pickup }) => {
     const collector = new ParameterCollector(config.allWorkoutParams);
     parameters += collector.getSearchString();
     return (
-        <div className='funnel-wrapper'>
-            <div className='category-wrapper'>
-                <div className='d-grid'>
-                    <Header>{ question }</Header>
-                    <hr/>
-                    <div>
-                    {
-                        funnel.map((category) => {
-                            let forward = '';
-                            if('?' !== parameters.charAt(parameters.length - 1)) {
-                                forward += '&';
-                            }
-                            forward = (parameters + forward + `${parameter}=${category.id}`);
-                            let furtherClasses = '';
-                            if(category.classes) {
-                                furtherClasses = ' ' + category.classes;
-                            }
-                            return (
-                                <div className='d-grid'>
-                                    <Button key={category.id} href={forward}
-                                        color={category.color} classes={`my-3 ${furtherClasses}`}
-                                        icon={category.icon} text={category.name}>
-                                    </Button>
-                                    {
-                                        category.subtitle && (<Subtitle text={category.subtitle} classes='mb-5' />)
-                                    }
-                                </div>
-                            );
-                        })
-                    }
-                    </div>
-                    <hr/>
-                    {
-                        !start && <ModeSelect />
-                    }
+        <div className='category-wrapper'>
+            <div className='d-grid'>
+                <Header>{ question }</Header>
+                <hr/>
+                <div>
+                {
+                    funnel.map((category) => {
+                        let forward = '';
+                        if('?' !== parameters.charAt(parameters.length - 1)) {
+                            forward += '&';
+                        }
+                        forward = (parameters + forward + `${parameter}=${category.id}`);
+                        let furtherClasses = '';
+                        if(category.classes) {
+                            furtherClasses = ' ' + category.classes;
+                        }
+                        return (
+                            <div className='d-grid'>
+                                <Button key={category.id} href={forward}
+                                    color={category.color} classes={`my-3 ${furtherClasses}`}
+                                    icon={category.icon} text={category.name}>
+                                </Button>
+                                {
+                                    category.subtitle && (<Subtitle text={category.subtitle} classes='mb-5' />)
+                                }
+                            </div>
+                        );
+                    })
+                }
                 </div>
+                <hr/>
+                {
+                    !start && <ModeSelect />
+                }
             </div>
         </div>
     );
