0330bc9d5696813996adc403a50d9bd531258865
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-09 14:47:11 +0100

    WIP: lazy/obfuscatable image class

---
+++ src/components/Image.js
@@ -0,0 +1,45 @@
+import { Component } from 'react'
+import { config } from '../assets/app.config.json'
+
+class Image extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            src: undefined
+        };
+    }
+
+    componentDidMount() {
+        let { url } = this.props;
+        if(config.obfuscateUrl) {
+            const fn = (e) => {
+                let { currentTarget:{ response } } = e;
+                const urlCreator = window.URL || window.webkitURL;
+                const url = urlCreator.createObjectURL(response);
+                this.setState({ imageUrl: url });
+            }
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.responseType = "blob";
+            xhr.onload = fn;
+            xhr.send();
+        } else {
+            this.setState({ src: url });
+        }
+    }
+
+    render() {
+        const { alt } = this.props;
+        const { src } = this.state;
+        return (
+            <>
+            {
+                src && (<img className={ config.style.imageClasses } alt={ alt } src={ src } />)
+            }
+            </>
+        );
+    }
+};
+
+export default Image;
