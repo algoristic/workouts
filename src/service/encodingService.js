c0fae41a18a79c4b85afcf37e1b51f58e3484495
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-14 18:13:34 +0100

    Configure schemas and make encoding configurable

--- src/service/encodingService.js
+++ src/service/encodingService.js
@@ -1,16 +1,26 @@
 import base64 from 'base-64'
 import utf8 from 'utf8'
 
+import { config } from '../assets/app.config.json'
+
 const encode = (string) => {
-    const bytes = utf8.encode(string);
-    const encoded = base64.encode(bytes);
-    return encoded;
+    if(config.encode) {
+        const bytes = utf8.encode(string);
+        const encoded = base64.encode(bytes);
+        return encoded;
+    } else {
+        return string;
+    }
 }
 
 const decode = (string) => {
-    const bytes = base64.decode(string);
-    const decoded = utf8.decode(bytes);
-    return decoded;
+    if(config.encode) {
+        const bytes = base64.decode(string);
+        const decoded = utf8.decode(bytes);
+        return decoded;
+    } else {
+        return string;
+    }
 }
 
 export {
--- src/service/schemaService.js
+++ src/service/schemaService.js
@@ -1,42 +1,25 @@
 import { config } from '../assets/app.config.json'
 
-const { schemas:{ programIdSchema, programPathSchema, workoutIdSchema, workoutPathSchema } } = config;
+const { mediaBase, programId, workoutId } = config;
 
 const program = (id, day) => {
-    return programIdSchema.replace('{program}', id).replace('{day}', day);
+    return programId.replace('{program}', id).replace('{day}', day);
 };
 
 const workout = (id) => {
-    return workoutIdSchema.replace('{workout}', id);
+    return workoutId.replace('{workout}', id);
 };
 
 const programPath = (id, day) => {
-    return programPathSchema.replace('{program}', id).replace('{day}', day);
+    return (mediaBase + program(id, day));
 };
 
 const workoutPath = (id) => {
-    return workoutPathSchema.replace('{workout}', id);
+    return (mediaBase + workout(id));
 };
 
 const resolvePath = (string) => {
-    /*
-        IDEE: die Methoden program() und workout() speichern beim ersten Aufruf
-        die IDs im localStorage unter den Objekten "workouts" und "programs".
-        Der string hier wird dann für alle Objekte abgeglichen via string.include().
-        Ist natürlich scheiße - dafür sollte ich mir wirklich etwas besseres ausdenken...
-    */
-    const parts = string.split(':');
-    const determinator = parts[0];
-    const identifier = parts[1];
-    const quantifier = parts[2];
-    switch(determinator) {
-        case 'p':
-            return programPath(identifier, quantifier)
-        case 'w':
-            return workoutPath(identifier);
-        default:
-            return undefined;
-    }
+    return (mediaBase + string);
 };
 
 export {
