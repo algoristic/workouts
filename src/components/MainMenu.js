99fbc38d219010f9930f1743e05d306640d2a5d6
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-14 20:49:10 +0100

    Add brand

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,27 +2,27 @@ import { config } from '../assets/app.config.json'
 
 const MainMenu = ({ app }) => {
     return (
-        <nav className={`navbar ${ config.style.navbarStyleClasses }`}>
-            <div className={ config.style.appContainer }>
-                <div className={ config.style.navbarClasses }>
-                    <span class='navbar-brand'></span>
-                    <button class='navbar-toggler' type='button'
-                        data-bs-toggle='collapse'
-                        data-bs-target='#collapsibleNavbar'>
-                        <span class='navbar-toggler-icon'></span>
-                    </button>
-                </div>
-                 <div class='collapse navbar-collapse' id='collapsibleNavbar'>
-                     <ul className='navbar-nav'>
-                         <li className='nav-item'>
-                             <a className={`nav-link ${(app && (app.id === config.apps.start) ? 'disabled' : '')} ${config.style.navbarButtonClasses}`}
-                                href={`?${config.parameters.app}=${config.apps.start}`}>
-                                Hauptmenü
-                             </a>
-                         </li>
-                     </ul>
-                 </div>
-            </div>
+        <nav className={`navbar ${config.style.navbarStyleClasses} ${config.style.appContainer}`}>
+        <div className={config.style.navbarClasses}>
+            <a href='/' class='navbar-brand'>
+                <img src='/favicon.png' style={{ width: '40px' }} />
+            </a>
+            <button class='navbar-toggler' type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#collapsibleNavbar'>
+                <span class='navbar-toggler-icon'></span>
+            </button>
+        </div>
+         <div class='collapse navbar-collapse' id='collapsibleNavbar'>
+             <ul className='navbar-nav'>
+                 <li className='nav-item'>
+                     <a className={`nav-link ${(app && (app.id === config.apps.start) ? 'disabled' : '')} ${config.style.navbarButtonClasses}`}
+                        href={`?${config.parameters.app}=${config.apps.start}`}>
+                        Hauptmenü
+                     </a>
+                 </li>
+             </ul>
+         </div>
         </nav>
     );
 };
