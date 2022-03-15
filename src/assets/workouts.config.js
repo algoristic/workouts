aa09609c95f037ce40e145ad85f6b068e1aa4c97
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-03-15 10:58:21 +0100

    More eays workout config

--- src/assets/workouts.config.js
+++ src/assets/workouts.config.js
@@ -7,78 +7,81 @@ const { types:{ strength, cardio, hiit, fight } } = values;
 const { levels:{ easy, normal, hard, extreme } } = values;
 
 //programs
-const militaryFit = "military-fit";
+const militaryFit = (day) => {
+    return program("military-fit", day);
+};
+
 //workouts
 const ptPyramid = "pt-pyramid-workout";
 
 const workouts = [
     {
-        "id": program(militaryFit, 1),
+        "id": militaryFit(1),
         "types": strength,
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 2),
+        "id": militaryFit(2),
         "types": strength,
         "levels": hard
     },
     {
-        "id": program(militaryFit, 3),
+        "id": militaryFit(3),
         "types": strength,
         "levels": extreme
     },
     {
-        "id": program(militaryFit, 4),
+        "id": militaryFit(4),
         "types": cardio,
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 4),
+        "id": militaryFit(4),
         "types": [cardio, fight],
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 5),
+        "id": militaryFit(5),
         "types": cardio,
         "levels": hard
     },
     {
-        "id": program(militaryFit, 6),
+        "id": militaryFit(6),
         "types": cardio,
         "levels": extreme
     },
     {
-        "id": program(militaryFit, 7),
+        "id": militaryFit(7),
         "types": fight,
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 8),
+        "id": militaryFit(8),
         "types": fight,
         "levels": hard
     },
     {
-        "id": program(militaryFit, 9),
+        "id": militaryFit(9),
         "types": fight,
         "levels": extreme
     },
     {
-        "id": program(militaryFit, 10),
+        "id": militaryFit(10),
         "types": [hiit, fight],
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 10),
+        "id": militaryFit(10),
         "types": hiit,
         "levels": [easy, normal]
     },
     {
-        "id": program(militaryFit, 11),
+        "id": militaryFit(11),
         "types": hiit,
         "levels": hard
     },
     {
-        "id": program(militaryFit, 12),
+        "id": militaryFit(12),
         "types": hiit,
         "levels": extreme
     },
